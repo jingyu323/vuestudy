68a73063c4e6e135ae057db3c4678385808677a4
jingyu323 <xzg323@163.com>
2022-12-16 14:17:05 +0800

    map

--- vuetest/src/http/httpRequestUtil.js
+++ vuetest/src/http/httpRequestUtil.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 // 全局引入
 
 var axiosIns = axios.create({
-  baseURL: 'https://easy-mock.com/mock/5c67cb3fadf6a5499fb8d31e/vuetest',
+  baseURL: 'https://easy-mock.com/vuetest',
   timeout: 1000,
   headers: {'X-Custom-Header': 'foobar'}
 })
