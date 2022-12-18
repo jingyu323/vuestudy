a25be119f74a1fd9e8231a15b7fd0f15e0226ca2
jingyu323 <xzg323@163.com>
2022-12-18 14:49:25 +0800

    准备铁路地图

--- vuetest/src/components/map/echartRailwayMap.vue
+++ vuetest/src/components/map/echartRailwayMap.vue
@@ -15,6 +15,7 @@ import xian from "@/assets/mapJson/xian.json";
 import xianyang from "@/assets/mapJson/xianyang.json";
 import china from "@/assets/mapJson/china.json";
 import binzhoushi from "@/assets/mapJson/binzhoushi.json";
+import {AREANAME} from "@/data/areaName.js";
 export default {
   name: "echartdot",
   data() {
@@ -36,7 +37,39 @@ export default {
     randomData: function () {
       return Math.round(Math.random() * 500);
     },
+    convertData :function(data) {
+    var res = [];
+    var geoCoordMap = AREANAME;//AREANAME为获取的js中数据
+    for (var i = 0; i < data.length; i++) {
+        var dataItem = data[i];
+        var fromCoord = geoCoordMap[dataItem[0].name];
+        var toCoord = geoCoordMap[dataItem[1].name];
+        if (fromCoord && toCoord) {
+            res.push({
+                fromName: dataItem[0].name,
+                toName: dataItem[1].name,
+                coords: [toCoord, fromCoord]
+            });
+        }
+    }
+    console.log(res);
+    return res;
+},  
+
     dotChart: function () {
+      console.log(AREANAME);
+      var mapTipList = [{
+   	  itemname:'京杭铁路',
+   	  itemcode:'1111',
+   	  compname:'10001',
+   	  totalinvestment:'200',
+   	  yearinvestplan:'2000',
+   	  yearinvestcomplete:'333'
+    }];
+
+    var BJData = [[{'name': '广东'}, {'name': '贵州'}],[{'name': '山西'}, {'name': '新疆'}],[{'name': '吉林'}, {'name': '广西壮族自治区'}],[{'name': '内蒙古自治区'}, {'name': '西藏'}],[{'name': '北京'}, {'name': '香港特別行政区'}],[{'name': '福建'}, {'name': '云南'}]];
+
+    var path = this.convertData(BJData);
       // json及registerMap的方式
       var mydata = [
         { name: '北京', value: '100' }, { name: '天津', value: this.randomData() },
@@ -102,6 +135,30 @@ export default {
                   { name: "山东", value: [117.00092, 36.675808,5000] },
                 ],
           },
+          ,{//白线
+			        name: '白线',
+			        type: 'lines',
+			        selectedMode: 'single',     //添加点击事件 此处必须设置
+			        zlevel: 2,//zlevel 大的 Canvas 会放在 zlevel 小的 Canvas 的上面。
+			        effect: {
+			            show: true,//是否显示特效
+			            period: 6,//特效动画时间
+			            trailLength: 0.7,//特效尾迹的长度。取从 0 到 1 的值，数值越大尾迹越长。
+			            color: '#F3B329',
+			            symbolSize: 1//线两端的标记大小，可以是一个数组分别指定两端，也可以是单个统一指定
+			        },
+			        symbol: ['none','diamond'],//无-->箭头使用arrow
+			        lineStyle: {
+			            normal: {
+			                color: '#fff',
+			                opacity: 0.3,
+			                width: 3,
+			                type:"dashed",//虚线
+			               // curveness: 0.2  //线条弧度 默认为直线
+			            }
+			        },
+			        data: path
+			    },
         ],
 
       }
