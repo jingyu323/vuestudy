bb95739b4337f6fd789ea628db5404767448051d
jingyu323 <xzg323@163.com>
2022-12-22 14:30:21 +0800

    调整身份文字显示

--- vuetest/src/components/map/echartRailwayMap.vue
+++ vuetest/src/components/map/echartRailwayMap.vue
@@ -4,13 +4,17 @@
     <h2>China Railway Map</h2>
     <button @click="goback()">返回</button>
     <DataGrid :data="data" style="height:250px">
-            <GridColumn field="itemid" title="Item ID"></GridColumn>
-            <GridColumn field="name" title="Name"></GridColumn>
-            <GridColumn field="listprice" title="List Price" align="right"></GridColumn>
-            <GridColumn field="unitcost" title="Unit Cost" align="right"></GridColumn>
-            <GridColumn field="attr" title="Attribute" width="30%"></GridColumn>
-            <GridColumn field="status" title="Status" align="center"></GridColumn>
-        </DataGrid>
+      <GridColumn field="itemid" title="Item ID"></GridColumn>
+      <GridColumn field="name" title="Name"></GridColumn>
+      <GridColumn
+        field="listprice"
+        title="List Price"
+        align="right"
+      ></GridColumn>
+      <GridColumn field="unitcost" title="Unit Cost" align="right"></GridColumn>
+      <GridColumn field="attr" title="Attribute" width="30%"></GridColumn>
+      <GridColumn field="status" title="Status" align="center"></GridColumn>
+    </DataGrid>
     <div id="container" style="height: 100%"></div>
     <!-- <Dialog ref="d1"
         :title="'Draggable And Resizable'"
@@ -32,138 +36,44 @@ import xianyang from "@/assets/mapJson/xianyang.json";
 import china from "@/assets/mapJson/china.json";
 import binzhoushi from "@/assets/mapJson/binzhoushi.json";
 import { AREANAME } from "@/data/areaName.js";
-import $ from 'jquery';
+import $ from "jquery";
 export default {
   name: "echartdot",
   data() {
     return {
-      msg: "this is  echartdot  file ",
-     
-       
+      msg: "this is  echartdot  file "
     };
   },
   methods: {
-    goback: function () {
+    goback: function() {
       window.history.back();
-    }, 
-    randomData: function () {
+    },
+    randomData: function() {
       return Math.round(Math.random() * 500);
     },
-    mapModal: function (start, end) {
-      let _this = this;
-      layui.use('layer', function () {
-        // if(layerMap != null){
-        // 	layer.close(layerMap);
-        // };
-        var layerMap = layer.open({
-          title: start + '-' + end + '电铁配套工程',
-          type: 1,
-          offset: ['67%', '14.5%'],
-          shade: 0,
-          skin: 'demo-class',//设置标题背景色为白色
-          content: `
-				<div  style="width:100%; height:400px;background-color:#FFF;  "> 
-          <table id="mapGrid" class="easyui-datagrid"  ></table>
-				</div>
-			`,
-          area: ['54%', '30%']
-        })
-        layer.ready(function () {
-          //初始化表头
-          _this.initMaptable("repId222");
-        });
-      });
-    },
-    initMaptable: function (repId) {
-      console.log("repId="+repId);
-      //获取表头  此处封装了ajax从数据库获取表头
-      var finaltitle = { "leftcols": "leftcols11", "rightcols": "rightcols33" };
-      var leftcols = finaltitle.leftcols;
-      var rightcols = finaltitle.rightcols;
- 
-       
-
-    },
-    convertData: function (data) {
-      var res = [];
-      var geoCoordMap = AREANAME;//AREANAME为获取的js中数据
-      for (var i = 0; i < data.length; i++) {
-        var dataItem = data[i];
-        var fromCoord = geoCoordMap[dataItem[0].name];
-        var toCoord = geoCoordMap[dataItem[1].name];
-        if (fromCoord && toCoord) {
-          res.push({
-            fromName: dataItem[0].name,
-            toName: dataItem[1].name,
-            coords: [toCoord, fromCoord]
-          });
-        }
-      }
-      console.log(res);
-      return res;
-    },
-
-    dotChart: function () {
-      console.log(AREANAME);
- 
-
-      var BJData = [[{ 'name': '广东' }, { 'name': '贵州' }], [{ 'name': '山西' }, { 'name': '新疆' }], [{ 'name': '吉林' }, { 'name': '广西壮族自治区' }], [{ 'name': '内蒙古自治区' }, { 'name': '西藏' }], [{ 'name': '北京' }, { 'name': '香港特別行政区' }], [{ 'name': '福建' }, { 'name': '云南' }]];
-
-      var path = this.convertData(BJData);
-      // json及registerMap的方式
-      var mydata = [
-        { name: '北京', value: '100' }, { name: '天津', value: this.randomData() },
-      ];
-      console.log("ssss" + beijing);
+    dotChart: function() {
       //  var echarts = require('echarts');
       var myChart = echarts.init(document.getElementById("container"));
-      echarts.registerMap('china', china, {});
+      echarts.registerMap("china", china, {});
       var option = {
-        tooltip: {
-          formatter: function (params, ticket, callback) {
-            if (params.seriesIndex == 0) {//当鼠标放在城市圈上不显示提示信息
-              return '';
-            }
-            //只有悬浮铁路才会出现提示框
-            var start = params.data.fromName;
-            var end = params.data.toName;
-            //这里的四个数据 需要从后台获取 此处写ajax就好
-            var gjxmsltip = 10;
-            var ztjtip = 20;
-            var bntzjhtip =30;
-            var tzwctip = 40;
-
-            let res = "<div>" +
-              "<p style='padding-left:10px;padding-right:20px;font-size:0.07rem;height:0.25rem;line-height:0.25rem;background:#4CA6A6;color:white;'>" + start + "-" + end + "电铁配电工程</p>"
-              + "<div style='margin:10px;color:#000;'>"
-              + "<p>挂接项目数量：" + gjxmsltip + "</p>"
-              + "<p>总投资：" + ztjtip + "万元</p>"
-              + "<p>本年投资计划：" + bntzjhtip + "万元</p>"
-              + "<p>投资完成：" + tzwctip + "万元</p>"
-              + "</div>"
-              + "</div>";
-            return res;
-          },
-          backgroundColor: 'white',
-          padding: 0
-        },
+         
         geo: {
-          map: 'china',
-          roam: false,
-          zoom: 1.23,
+          map: "china",
+          zoom: 4,
+          roam: true,
           label: {
             emphasis: {
               show: false,
-              color: '#fff'
+              color: "#fff"
             }
           },
           itemStyle: {
             normal: {
-              borderColor: '#DCDFE1',//省边界颜色
-              areaColor: '#8FCAF1'//地图颜色
+              borderColor: "#DCDFE1", //省边界颜色
+              areaColor: "#8FCAF1" //地图颜色
             },
             emphasis: {
-              areaColor: '#F3B329'//懸浮顏色 
+              areaColor: "#F3B329" //懸浮顏色
             }
           }
         },
@@ -171,25 +81,25 @@ export default {
         series: [
           //圆圈
           {
-            name: '城市',
-            type: 'effectScatter',//圆圈涟漪
-            coordinateSystem: 'geo',
+            name: "城市",
+            type: "effectScatter", //圆圈涟漪
+            coordinateSystem: "geo",
             zlevel: 3,
             rippleEffect: {
-              brushType: 'stroke'
+              brushType: "stroke"
             },
             label: {
               normal: {
                 show: true,
-                position: 'right',
-                formatter: '{b}',
-                color: '#fff'
+                position: "right",
+                formatter: "{b}",
+                color: "#fff"
               }
             },
-            symbolSize: 8,//圆圈大小
+            symbolSize: 8, //圆圈大小
             itemStyle: {
               normal: {
-                color: 'yellow'//圆圈颜色
+                color: "yellow" //圆圈颜色
               }
             },
 
@@ -210,77 +120,77 @@ export default {
               { name: "福建", value: [119.30624, 26.075302, 3900] },
               { name: "云南", value: [102.71225, 25.04061, 3700] },
               { name: "海南", value: [110.33119, 20.031971, 3400] },
-              { name: "台湾", value: [121.50906, 25.044333, 3300] },
-            ],
+              { name: "台湾", value: [121.50906, 25.044333, 3300] }
+            ]
           },
-          , {//白线
-            name: '白线',
-            type: 'lines',
-            selectedMode: 'single',     //添加点击事件 此处必须设置
-            zlevel: 2,//zlevel 大的 Canvas 会放在 zlevel 小的 Canvas 的上面。
+          ,
+          {
+            //白线
+            name: "白线",
+            type: "lines",
+            selectedMode: "single", //添加点击事件 此处必须设置
+            zlevel: 2, //zlevel 大的 Canvas 会放在 zlevel 小的 Canvas 的上面。
             effect: {
-              show: true,//是否显示特效
-              period: 6,//特效动画时间
-              trailLength: 0.7,//特效尾迹的长度。取从 0 到 1 的值，数值越大尾迹越长。
-              color: '#F3B329',
-              symbolSize: 1//线两端的标记大小，可以是一个数组分别指定两端，也可以是单个统一指定
+              show: true, //是否显示特效
+              period: 6, //特效动画时间
+              trailLength: 0.7, //特效尾迹的长度。取从 0 到 1 的值，数值越大尾迹越长。
+              color: "#F3B329",
+              symbolSize: 1 //线两端的标记大小，可以是一个数组分别指定两端，也可以是单个统一指定
             },
-            symbol: ['none', 'diamond'],//无-->箭头使用arrow
+            symbol: ["none", "diamond"], //无-->箭头使用arrow
             lineStyle: {
               normal: {
-                color: '#fff',
+                color: "#fff",
                 opacity: 0.3,
                 width: 3,
-                type: "dashed",//虚线
+                type: "dashed" //虚线
                 //  curveness: 0.2  //线条弧度 默认为直线
               }
             },
             data: path
           },
-          , {//黑线
-            name: '黑线',
-            type: 'lines',
-            selectedMode: 'single',     //添加点击事件 此处必须设置
+          ,
+          {
+            //黑线
+            name: "黑线",
+            type: "lines",
+            selectedMode: "single", //添加点击事件 此处必须设置
             zlevel: 1,
             effect: {
               show: true,
               period: 6,
               trailLength: 0.7,
-              color: '#aaa',
+              color: "#aaa",
               symbolSize: 1
             },
-            symbol: ['none', 'diamond'],//arrow
+            symbol: ["none", "diamond"], //arrow
             lineStyle: {
               normal: {
-                color: '#000',
+                color: "#000",
                 opacity: 0.3,
-                width: 3,
+                width: 3
               }
             },
             data: path
           }
-        ],
-
-      }
+        ]
+      };
       myChart.setOption(option, true);
       let _this = this;
-      myChart.on('click', function (params) {
-        console.log("param : === " + params);//此处写点击事件内容
+      myChart.on("click", function(params) {
+        console.log("param : === " + params); //此处写点击事件内容
         if (params.componentType == "series") {
           var start = params.data.fromName;
           var end = params.data.toName;
           _this.mapModal(start, end);
         }
       });
-
     }
   },
   mounted() {
     this.dotChart();
-  },created(){
-     
-    
-  }
+  },
+  created() {}
 };
 </script>
 
